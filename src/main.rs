d9549df5c65441c572165b3da1d7334cdbb3a47b
EmperorOrokuSaki <me@nimara.xyz>
2025-05-07 14:54:29 +0200

    feat: basic cli and toml config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use std::{fs, path::PathBuf};
+
+use clap::Parser;
+use serde::Deserialize;
+
+#[derive(Parser)]
+#[command(version, about)]
+struct Cli {
+    /// Config TOML file
+    pub config: PathBuf,
+}
+
+/// Config TOML
+#[derive(Deserialize)]
+pub struct Config {
+    addresses: Addresses,
+}
+
+/// Secret Addresses
+#[derive(Deserialize)]
+struct Addresses {
+    execution_pk: String,
+    validation_pk: String,
+}
+
+fn main() -> anyhow::Result<()> {
+    let cli = Cli::parse();
+
+    let raw_config = fs::read_to_string(cli.config)?;
+
+    let config: Config = toml::from_str(&raw_config)?;
+
+    println!("{}", config.addresses.execution_pk);
+
+    Ok(())
 }
