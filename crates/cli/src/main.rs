edb23602ec11d250d8d01bde23fc4ee7a2850507
ozwaldorf <self@ossian.dev>
2025-08-14 15:29:52 -0400

    feat: filter workspace crates separately for -v/-vv/-vvv, all at info by default, all at trace for -vvvv

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -3,14 +3,15 @@ use std::path::PathBuf;
 use alloy::signers::local::PrivateKeySigner;
 use clap::{ArgAction, Parser};
 use color_eyre::eyre::{bail, Context, Result};
-use tracing::{info, instrument, warn};
+use tracing::{info, trace};
 use tracing_subscriber::EnvFilter;
+use workspace_filter::workspace_filter;
 
 mod commands;
 mod config;
 
 #[derive(Parser)]
-#[command(author, version, about, long_about = None)]
+#[command(author, version, about)]
 pub(crate) struct Args {
     /// Path to config file
     #[arg(short, long, global = true, display_order(0))]
@@ -18,8 +19,15 @@ pub(crate) struct Args {
     /// Ethereum private keys to use
     #[arg(long, global = true, action(ArgAction::Append), display_order(0))]
     pub pk: Option<Vec<String>>,
-    /// Increases the level of verbosity (the max level is -vvv).
+    /// Increases the level of verbosity. Max value is -vvvv.
+    ///
+    /// * Default: All crates at info level
+    /// * -v     : Nomad crates at debug level, all others at info
+    /// * -vv    : Nomad crates at trace level, all others at info
+    /// * -vvv   : Nomad crates at trace level, all others at debug
+    /// * -vvvv  : All crates at trace level
     #[arg(short, global = true, action = ArgAction::Count, display_order(99))]
+    #[clap(verbatim_doc_comment)]
     pub verbose: u8,
 
     #[command(subcommand)]
@@ -46,39 +54,45 @@ impl Args {
             })
             .collect()
     }
+
+    // Setup logging filters and subscriber
+    pub fn setup_logging(&self) {
+        let filter = std::env::var("RUST_LOG").unwrap_or_else(|_| {
+            // Default which is directed by the verbosity flag
+            match self.verbose {
+                0 => "info".into(),
+                1 => workspace_filter!("debug", "info,nomad={level}"),
+                2 => workspace_filter!("trace", "info,nomad={level}"),
+                3 => workspace_filter!("trace", "debug,nomad={level}"),
+                _ => "trace".into(),
+            }
+        });
+        let filter = EnvFilter::builder().parse_lossy(filter);
+        let env_filter = filter.to_string();
+
+        // Init subscriber
+        let _ = tracing_subscriber::fmt()
+            .with_env_filter(filter)
+            .with_target(self.verbose > 1)
+            .with_thread_ids(false)
+            .with_file(false)
+            .with_line_number(false)
+            .compact()
+            .try_init();
+
+        trace!(env_filter);
+    }
+
+    // Setup and execute the given command
+    pub async fn execute(self) -> Result<()> {
+        color_eyre::install()?;
+        self.setup_logging();
+        let config = config::Config::load(self.config.as_ref())?;
+        let signers = self.build_signers()?;
+        self.cmd.execute(config, signers).await
+    }
 }
 
-#[tokio::main]
-#[instrument]
-async fn main() -> Result<()> {
-    color_eyre::install()?;
-
-    // Parse cli arguments and app setup
-    let args = Args::parse();
-
-    // Setup logging filters
-    let env_filter = EnvFilter::builder().parse_lossy(match std::env::var("RUST_LOG") {
-        // Environment override
-        Ok(filter) => filter,
-        // Default which is directed by the verbosity flag
-        Err(_) => match args.verbose {
-            0 => "info",
-            1 => "debug",
-            _ => "trace",
-        }
-        .to_string(),
-    });
-    let _ = tracing_subscriber::fmt()
-        .with_env_filter(env_filter)
-        .with_target(true)
-        .with_thread_ids(false)
-        .with_file(false)
-        .with_line_number(false)
-        .compact()
-        .try_init();
-
-    // Load config, build eth signers, and run the given command
-    let config = config::Config::load(args.config.as_ref())?;
-    let signers = args.build_signers()?;
-    args.cmd.execute(config, signers).await
+fn main() -> Result<()> {
+    tokio::runtime::Runtime::new()?.block_on(Args::parse().execute())
 }
