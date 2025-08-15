3a040580fe2166bc430e51b498167ffa7b649875
ozwaldorf <self@ossian.dev>
2025-08-15 15:36:28 -0400

    feat: nix devshell

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    foundry.url = "github:shazow/foundry.nix/stable";
+  };
+
+  outputs =
+    inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = import inputs.nixpkgs {
+        inherit system;
+        overlays = [ inputs.foundry.overlay ];
+      };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          # Build
+          rustup
+          openssl
+          pkgconf
+
+          # Development
+          foundry
+          jq
+        ];
+      };
+    };
+}
