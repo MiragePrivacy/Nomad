fe6a8a92b856d933eb9ac5c90adea21ae899c0b4
ozwaldorf <self@ossian.dev>
2025-09-27 09:35:17 -0400

    feat: check fortanix target in ci, update build script

--- crates/enclave/build.sh
+++ crates/enclave/build.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
+set -e
 
 ROOT=$(cargo metadata --format-version 1 | jq .workspace_root -r)
-cd $ROOT/crates/enclave
+cd $ROOT
 
 # We compute the number of threads dynamically from spawn calls in the code.
 # The build script is included for counting the main thread.
-THREADS=$(find . -type f -exec grep -o "std::thread::spawn" {} + | wc -l)
+THREADS=1
 STACK_SIZE=0x200000 # 2 MiB (rust default)
 HEAP_SIZE=0x20000000 # 512 MiB
 
 echo "[Stage 1] Building fortanix enclave"
-cargo +nightly build --locked --release --target x86_64-fortanix-unknown-sgx
+cargo +nightly build -p nomad-enclave --locked --release --target x86_64-fortanix-unknown-sgx
 BUILD_OUTPUT="$ROOT/target/x86_64-fortanix-unknown-sgx/release/nomad-enclave"
 
 echo "[Stage 2] Converting to SGXS (threads=$THREADS, stack=$STACK_SIZE, heap=$HEAP_SIZE)"
@@ -18,8 +19,8 @@ ftxsgx-elf2sgxs --threads $THREADS --stack-size $STACK_SIZE --heap-size $HEAP_SI
 SGXS_OUTPUT="$BUILD_OUTPUT.sgxs"
 
 VERSION=$(cargo metadata --format-version 1 | jq '.packages[] | select(.name == "nomad-enclave") | .version' -r)
-DEST="${1:-./nomad-enclave-$VERSION.sgxs}"
-cd -
-mv $SGXS_OUTPUT $DEST -v
+FINAL_OUTPUT="$BUILD_OUTPUT-$VERSION.sgxs"
+mv $SGXS_OUTPUT $FINAL_OUTPUT
+mv $FINAL_OUTPUT ${1:-.} -v
 
 # TODO: sign the enclave too
